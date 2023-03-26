ce645c29dc53eabec047135dc77a3f229132d78c
treyjaded <trevonjcharles@gmail.com>
2023-03-26 00:07:47 -0400

    added apiKey

---
+++ apiKey.js
@@ -0,0 +1,5 @@
+const config ={
+            trevonApiKey:'BQD9yg-sMBhr9-HbsD9HsorTzuJZkA7caa7y-oTqmZCihvbCrTOfzhcugVFBdBR4HDowh4T4nhBAv2utb29LJX7ZPMmeWCikgf8ujU42ancdEMPCTwj9MJMzu4RHwQ9PUp8glq-M7ooA2xxG83QH278hUBdP2luapTFfSwzUrtTiWlOiZy5GuestLQ',
+            trevonSecertApiKey: '0a9415cd75634018bcfce83622defd36',
+}
+export default config 
