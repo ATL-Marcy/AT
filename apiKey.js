b291acc0c7617ecd47d5c6fff00861ae2481b932
Afairman2003 <afairman@brooklynsteamcenter.org>
2023-03-27 17:14:02 -0400

    changes made on amanda branch

--- apiKey.js
+++ apiKey.js
@@ -1,5 +1,9 @@
 const config ={
-            trevonApiKey:'BQD9yg-sMBhr9-HbsD9HsorTzuJZkA7caa7y-oTqmZCihvbCrTOfzhcugVFBdBR4HDowh4T4nhBAv2utb29LJX7ZPMmeWCikgf8ujU42ancdEMPCTwj9MJMzu4RHwQ9PUp8glq-M7ooA2xxG83QH278hUBdP2luapTFfSwzUrtTiWlOiZy5GuestLQ',
-            trevonSecertApiKey: '0a9415cd75634018bcfce83622defd36',
+            trevonClientId:"8000899d5e9e48a4b3247707457bda87",
+            trevonSecertClientId: '0a9415cd75634018bcfce83622defd36',
+            amandaClientId: "7a48cb940a244a798500cc4b69eedb42",
+            amandaSecretClientId: "d698d0aee7d04d07a85a9bd101095d35",
 }
 export default config 
+
+
