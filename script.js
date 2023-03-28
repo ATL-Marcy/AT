6f8803e67378b111675c1d6b065d0b785cabb0ac
treyjaded <trevonjcharles@gmail.com>
2023-03-28 14:04:35 -0400

    Merge Access Token

--- script.js
+++ script.js
@@ -1,52 +1,93 @@
 import config from "./apiKey.js"
-
-const treyClientId = config.trevonClientId //Trevon ClientId
-const treySecertClientId = config.trevonSecertClientId
-//Amanda's Token
+//Trevon Client Id==================================
+const clientId = config.trevonClientId 
+const secertClientId = config.trevonSecertClientId
+//Amanda's Client Id================================
 const amandaClientId= config.amandaClientId
 const amandaSecretClientId = config.amandaSecretClientId
-console.log(amandaClientId)
-//console.log(treyClientId)
 
 
-/*
-1. Create HTML elements (give them ids)
-2. Create variables for those DOM elements in JavaScript
-3. Fetch data
-4. Convert from JSON
-5. Use the data to manipulate the DOM elements.
-*/
-
-//Helper Fetch Function 
-
-// const clientid = treyToken
-// const clientSecret = treySecert
-// // private methods
-// const _getToken = async () => {
-// const result = await fetch(`https://accounts.spotify.com/api/token`,{
-//     method: 'POST',
-//     headers: {
-//     'Content-Type': 'application/x-www-form-urlencoded',
-//     'Authorization' : 'Basic' + btoa(clientid + ':' + clientSecret)
-//     },
-//     body : 'grant_type = client_credentials'
-//     })
-
-// const data  = await result.json();
-// console.log(data)
-// return data.access_token;
-// }
-// _getToken()
 
 
-const endpoint = "https://api.spotify.com/v1/recommendations";
-const artists = '6sFIWsNpZYqfjUpaCgueju';
-const danceability = encodeURIComponent('0.9');
+
+
+
+//======Obtains parameters from the hash of the URL========
+  
+function getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+    q = window.location.hash.substring(1);
+    while ( e = r.exec(q)) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+  }
+  var stateKey = 'spotify_auth_state';
+  var params = getHashParams();
+//ACCESS TOKEN=========================
+  var access_token = params.access_token,
+      state = params.state,
+      storedState = localStorage.getItem(stateKey);
+//============================
+(function() {
+
+ //==== Generates a random string containing numbers and letters==
+ 
+  function generateRandomString(length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  };
+
+  // var userProfileSource = document.getElementById('user-profile-template').innerHTML,
+  //     userProfileTemplate = Handlebars.compile(userProfileSource),
+  //     userProfilePlaceholder = document.getElementById('user-profile');
+
+  //     oauthSource = document.getElementById('oauth-template').innerHTML,
+  //     oauthTemplate = Handlebars.compile(oauthSource),
+  //     oauthPlaceholder = document.getElementById('oauth');
+
+
+    document.getElementById('search-bar').addEventListener('click', function()  {
+      
+      const redirect_uri = `http://127.0.0.1:5500/AT/index.html`; // Your redirect uri
+     
+
+      var state = generateRandomString(16);
+
+      localStorage.setItem(stateKey, state);
+      var scope = 'user-read-private user-read-email';
+
+      var url = 'https://accounts.spotify.com/authorize';
+      url += '?response_type=token';
+      url += '&client_id=' + encodeURIComponent(clientId);
+      url += '&scope=' + encodeURIComponent(scope);
+      url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+      url += '&state=' + encodeURIComponent(state);
+
+      window.location = url;
+    }, false);
+  
+})();
+
+    
+
+    
+//Example Fetch ==============================
+
+// const endpoint = "https://api.spotify.com/v1/recommendations";
+// const artists = '6sFIWsNpZYqfjUpaCgueju';
+// const danceability = encodeURIComponent('0.9');
 
 // fetch(`${endpoint}?seed_artists=${artists}&target_danceability=${danceability}`, {
 //   method: "GET",
 //   headers: {
-//       Authorization: `Bearer ${treyToken}`
+//       Authorization: `Bearer ${access_token}`
 //   }
 // })
 // .then(response => response.json())
@@ -57,28 +98,26 @@ const danceability = encodeURIComponent('0.9');
 // })
 
 
-
+//Helper Function for obtaining Spotify DATA=======================
 const fetchFrom = async function (url){
     try {
         const response = await fetch(url, {
               method: "GET",
               headers: {
-                  Authorization: `Bearer BQDwJwzcm_dE4nfM1N5L4KqYzj30TWALIykXCn7a_GbvNI4bPm9vKZ79E4gJRIHI1JsW8fDkfQ9-UkrY2iG60YfF7is-QwR4idqO8k5e8Xc191VWZboH`
+                  Authorization: `Bearer ${access_token}
+                  `
               }
             })
         const data = await response.json();
-        //console.log(data);
+        // console.log(data);
         return data
     }
     catch(error){
         console.log("There was an error.",error);
     }
 }
-/*
-curl -X POST "https://accounts.spotify.com/api/token" \
-     -H "Content-Type: application/x-www-form-urlencoded" \
-     -d "grant_type=client_credentials&client_id=7a48cb940a244a798500cc4b69eedb42&client_secret=d698d0aee7d04d07a85a9bd101095d35"
-*/
+//============================================
+
 
 const spotifyUrl = 'https://api.spotify.com/v1/audio-features/11dFghVXANMlKmJXsNCbNl'
 const artistUrl = 'https://api.spotify.com/v1/tracks/11dFghVXANMlKmJXsNCbNl'
@@ -87,8 +126,19 @@ const urll = `https://api.spotify.com/v1/artists?ids=2CIMQHirSU0MQqyYHq0eOx%2C57
 
 async function search(){
     const data = await fetchFrom(spotifyUrl)
-    //const name = data.artists[0].name
-    console.log (data)
-   // console.log(name)
+    const name = data.acousticness
+    console.log(data)
+    console.log(name)
 }
 search()
+
+
+
+
+
+
+//===================AUTHORIZATION=============================================
+// curl -X POST "https://accounts.spotify.com/api/token" \
+//      -H "Content-Type: application/x-www-form-urlencoded" \
+//      -d "grant_type=client_credentials&client_id=8000899d5e9e48a4b3247707457bda87&client_secret=0a9415cd75634018bcfce83622defd36"
+//=========================================================================
