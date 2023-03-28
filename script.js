498a0a076b5ceacf316e725af2b5a54fda22bf50
treyjaded <trevonjcharles@gmail.com>
2023-03-28 13:20:46 -0400

    Access Token

--- script.js
+++ script.js
@@ -2,73 +2,89 @@ import config from "./apiKey.js"
 
 const clientId = config.trevonClientId //Trevon Token
 const client_secret = config.trevonSecertClientId
-console.log(clientId)
-
-/*
-1. Create HTML elements (give them ids)
-2. Create variables for those DOM elements in JavaScript
-3. Fetch data
-4. Convert from JSON
-5. Use the data to manipulate the DOM elements.
-*/
+// console.log(clientId)
 
 
+// function requestAuthorization(){
+//     const AUTHORIZE = "https://accounts.spotify.com/authorize"
+//     const client_id = document.getElementById("clientid")
+//     const client_secret = document.getElementById("secretid")
+//     localStorage.setItem("clientid", client_id)
 
-
-const redirect_uri = `http://127.0.0.1:5500/AT/index.html`
-
-
-function requestAuthorization(){
-    const AUTHORIZE = "https://accounts.spotify.com/authorize"
-    const client_id = document.getElementById("clientid")
-    const client_secret = document.getElementById("secretid")
-    localStorage.setItem("clientid", client_id)
-
-    let url = AUTHORIZE;
-    url += `?client_id=` + clientId;
-    url += "&response_type=code" 
-    url += "&redirect_uri=" + encodeURI(redirect_uri)
-    url += "&show_dialog=true"
-    url += "user-read-private user-read-user-top-read user-read-private playlist-modify-public playlist-modify-private user-read-playback-state user-modify-playback-state user-read-birthdate user-read-email"
-    window.location.href = url;
+//     let url = AUTHORIZE;
+//     url += `?client_id=` + clientId;
+//     url += "&response_type=code" 
+//     url += "&redirect_uri=" + encodeURI(redirect_uri)
+//     url += "&show_dialog=true"
+//     url += "user-read-private user-read-user-top-read user-read-private playlist-modify-public playlist-modify-private user-read-playback-state user-modify-playback-state user-read-birthdate user-read-email"
+//     window.location.href = url;
     
-}
-document.getElementById("button").addEventListener( "click", (e) => {
-    requestAuthorization()
-})
-
-
-
-
-// const go = async ()=> {
-//     const response = await fetch('https://accounts.spotify.com/api/token', {
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'application/x-www-form-urlencoded',
-//         'Authorization': 'Basic' + btoa(clientId + ":" + client_secret)
-//     },
-//     body: `grant_type=client_credentials`
-//     })
-//     const data = await response.json()
-//     console.log(data)
 // }
-// go()
 
-var auth = 'Basic ' + (clientId + ':' + client_secret).toString('base64');
-var data = 'grant_type=client_credentials'
-var options = {
-    hostname:'accounts.spotify.com',
-    path:'/api/token',
-    method: 'POST',
-    headers:{
-        'Authorization': auth,
-        'Accept':'application/json',
-        'Content-Type': 'application/x-www-form-urlencoded'
+
+
+
+//======Obtains parameters from the hash of the URL========
+   
+function getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+    q = window.location.hash.substring(1);
+    while ( e = r.exec(q)) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
     }
-};
+    return hashParams;
+  }
+  var stateKey = 'spotify_auth_state';
+  var params = getHashParams();
+//ACCESS TOKEN=========================
+  var access_token = params.access_token,
+      state = params.state,
+      storedState = localStorage.getItem(stateKey);
+//============================
+(function() {
+
+ //==== Generates a random string containing numbers and letters==
+ 
+  function generateRandomString(length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  };
+
+  // var userProfileSource = document.getElementById('user-profile-template').innerHTML,
+  //     userProfileTemplate = Handlebars.compile(userProfileSource),
+  //     userProfilePlaceholder = document.getElementById('user-profile');
+
+  //     oauthSource = document.getElementById('oauth-template').innerHTML,
+  //     oauthTemplate = Handlebars.compile(oauthSource),
+  //     oauthPlaceholder = document.getElementById('oauth');
 
 
+    document.getElementById('button').addEventListener('click', function()  {
+      
+      const redirect_uri = `http://127.0.0.1:5500/AT/index.html`; // Your redirect uri
 
+      var state = generateRandomString(16);
+
+      localStorage.setItem(stateKey, state);
+      var scope = 'user-read-private user-read-email';
+
+      var url = 'https://accounts.spotify.com/authorize';
+      url += '?response_type=token';
+      url += '&client_id=' + encodeURIComponent(clientId);
+      url += '&scope=' + encodeURIComponent(scope);
+      url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+      url += '&state=' + encodeURIComponent(state);
+
+      window.location = url;
+    }, false);
+  
+})();
 
     
 
@@ -82,7 +98,7 @@ var options = {
 // fetch(`${endpoint}?seed_artists=${artists}&target_danceability=${danceability}`, {
 //   method: "GET",
 //   headers: {
-//       Authorization: `Bearer ${treyToken}`
+//       Authorization: `Bearer ${access_token}`
 //   }
 // })
 // .then(response => response.json())
@@ -91,15 +107,15 @@ var options = {
 //     console.log(`${item.name} by ${item.artists[0].name}`);
 //   })
 // })
-//============================================
 
-//Helper Function================================
+
+//Helper Function for obtaining Spotify DATA=======================
 const fetchFrom = async function (url){
     try {
         const response = await fetch(url, {
               method: "GET",
               headers: {
-                  Authorization: `Bearer BQA-ySDYeNKdFpHsSXG_z15R9rUwMP0mwryIMfvj7BmAfq4T-81F4kVPRkMYQqNuyHTCoKAS9llVtFj9GdY8SIiAdWtTqxWSHIQpj7BtG-5mJt9H4M1i
+                  Authorization: `Bearer ${access_token}
                   `
               }
             })
@@ -136,51 +152,4 @@ search()
 // curl -X POST "https://accounts.spotify.com/api/token" \
 //      -H "Content-Type: application/x-www-form-urlencoded" \
 //      -d "grant_type=client_credentials&client_id=8000899d5e9e48a4b3247707457bda87&client_secret=0a9415cd75634018bcfce83622defd36"
-//==============================================================================
-/*
- async function generateCodeChallenge(codeVerifier) {
-        function base64encode(string) {
-          return btoa(String.fromCharCode.apply(null, new Uint8Array(string)))
-            .replace(/\+/g, '-')
-            .replace(/\//g, '_')
-            .replace(/=+$/, '');
-        }
-      
-        const encoder = new TextEncoder();
-        const data = encoder.encode(codeVerifier);
-        const digest = await window.crypto.subtle.digest('SHA-256', data);
-      
-        return base64encode(digest);
-      }
-    
-    generateCodeChallenge(codeVerifier).then(codeChallenge => {
-      let state = generateRandomString(16);
-      let scope = 'user-read-private user-read-email';
-    
-      localStorage.setItem('code-verifier', codeVerifier);
-    
-      let args = new URLSearchParams({
-        response_type: 'code',
-        client_id: client_id,
-        scope: scope,
-        redirect_uri: redirectUri,
-        state: state,
-        code_challenge_method: 'S256',
-        code_challenge: codeChallenge
-      });
-    
-      window.location = 'https://accounts.spotify.com/authorize?' + args;
-    });
-   
-  });
-    
-  const urlParams = new URLSearchParams(window.location.search);
-  let code = urlParams.get('code');
-  
-let body = new URLSearchParams({
-  grant_type: 'authorization_code',
-  code: code,
-  redirect_uri: redirectUri,
-  client_id: client_id,
-  code_verifier: codeVerifier
-*/
+//=========================================================================
